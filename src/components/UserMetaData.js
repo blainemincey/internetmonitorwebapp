eef195cd6aff606f73e68084880e3c047c1a6855
Blaine Mincey <blaine.mincey@gmail.com>
2019-08-04 14:40:15 -0400

    use meta data from auth0 rule

--- src/components/UserMetaData.js
+++ src/components/UserMetaData.js
@@ -8,14 +8,15 @@ class UserMetaData extends Component {
 
         this.state = {
             user: this.props.user,
-            userMetaData: ''
+            //userMetaData: ''
         }
 
-        this.loadUserMetaData = this.loadUserMetaData.bind(this);
-        this.auth0API = process.env.REACT_APP_AUTH0_API;
-        this.auth0Bearer = process.env.REACT_APP_AUTH0_BEARER;
+        //this.loadUserMetaData = this.loadUserMetaData.bind(this);
+        //this.auth0API = process.env.REACT_APP_AUTH0_API;
+        //this.auth0Bearer = process.env.REACT_APP_AUTH0_BEARER;
     }
 
+    /**
     async loadUserMetaData() {
         console.log("Load User Meta Data.");
         let url = this.auth0API + "%22" + this.state.user.email + "%22&search_engine=v3";
@@ -50,18 +51,20 @@ class UserMetaData extends Component {
             return false;
         }
     }
+     */
 
     async componentDidMount() {
-        await this.loadUserMetaData();
+        //await this.loadUserMetaData();
     }
 
     render() {
-        const {userMetaData} = this.state;
+        const {user} = this.state;
+        const stringtoreplace = "https://blaine-internetmonitor.mongodb.com/";
 
-        let myUserMetaData = '';
-        if(userMetaData.length > 0) {
-            myUserMetaData = userMetaData[0];
-        }
+        let json = JSON.stringify(user);
+        let jsonString = json.replace(new RegExp(stringtoreplace, 'gi'), "");
+
+        let myJson = JSON.parse(jsonString);
 
         return (
             <Container>
@@ -72,12 +75,22 @@ class UserMetaData extends Component {
                 </Row>
                 <Row>
                     <Col>
-                        <p>Last IP: {myUserMetaData.last_ip}</p>
+                        <p>Current IP: {myJson.ip}</p>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                        <p>Total Number Logins: {myUserMetaData.logins_count}</p>
+                        <p>Total Number Logins: {myJson.loginsCount}</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>Current City: {myJson.city}</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>Current Timezone: {myJson.timeZone}</p>
                     </Col>
                 </Row>
             </Container>
